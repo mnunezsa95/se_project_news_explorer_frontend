29cc516840bb7eb143c4cc35220061238aa0ceab
mnunezsa <mnunezsa95@gmail.com>
2023-10-16 15:13:21 -0400

    update close button bug on SuccessModal

--- src/components/SuccessModal/SuccessModal.js
+++ src/components/SuccessModal/SuccessModal.js
@@ -5,7 +5,7 @@ function SuccessModal({ modalName, handleCloseModal, onLoginModal }) {
   return (
     <div className={`modal modal__type-${modalName}`}>
       <div className={`modal__content-success`}>
-        <button className="modal__close-button" type="button" onClose={handleCloseModal}></button>
+        <button className="modal__close-button" type="button" onClick={handleCloseModal}></button>
         <h3 className={`modal__title-success`}>Registration successfully completed!</h3>
         <button className={`modal__redirect-button-success`} type="button" onClick={onLoginModal}>
           Sign in
