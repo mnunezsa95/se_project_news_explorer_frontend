ae3db7d2c29bf32e05c0fc36b679489424ee18ff
mnunezsa <mnunezsa95@gmail.com>
2023-10-17 09:45:21 -0400

    add show more button

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -4,13 +4,14 @@ import NewsCard from "../NewsCard/NewsCard";
 
 function NewsCardList() {
   return (
-    <section className="newscardlist-section">
+    <section className="newscardlist__section">
       <h2 className="newscardlist__title">Search results</h2>
       <div className="newscardlist__container">
         <NewsCard />
         <NewsCard />
         <NewsCard />
       </div>
+      <button className="newscardlist__button">Show more</button>
     </section>
   );
 }
