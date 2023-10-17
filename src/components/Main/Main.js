ccdc0cabf20930e8cda35979b3ea18972e25766c
mnunezsa <mnunezsa95@gmail.com>
2023-10-17 16:12:48 -0400

    add NothingFound component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,15 @@
 import React from "react";
-import "./Main.css";
 import NewsCardList from "../NewsCardList/NewsCardList";
+import NothingFound from "../NothingFound/NothingFound";
+import "./Main.css";
 
 function Main() {
-  return <NewsCardList />;
+  return (
+    <>
+      <NewsCardList />
+      <NothingFound />
+    </>
+  );
 }
 
 export default Main;
