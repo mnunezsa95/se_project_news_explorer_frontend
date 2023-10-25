64a7b4ab86496f333ae2d6a8cc0cde4a28c3a56f
mnunezsa <mnunezsa95@gmail.com>
2023-10-25 09:01:12 -0400

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 export const BASE_URL = "https://newsapi.org/v2/everything?";
 export const apiKey = "4bfe57c6a85a467cbe63a01fcc5e32fc";
-export const lang = navigator.language.split("-");
+export const lang = navigator.language.split("-"); // retrieves language from user browser
 
+// gets current date & formats it for api call
 function getcurrentDate() {
   const date = new Date();
   const year = date.toLocaleString("default", { year: "numeric" });
@@ -10,11 +11,11 @@ function getcurrentDate() {
   const formattedDate = year + "-" + month + "-" + day;
   return formattedDate;
 }
-
 export const currentDate = getcurrentDate();
 
+// gets current date, counts back 1 week, formats it for api call
 function getLastWeekDate() {
-  const time = new Date().getTime() - 604800000; // 604800000 = 1 weeek
+  const time = new Date().getTime() - 604800000; // 604800000ms = 1 weeek
   const previousWeek = new Date(time);
   const year = previousWeek.toLocaleString("default", { year: "numeric" });
   const month = previousWeek.toLocaleString("default", { month: "2-digit" });
@@ -22,14 +23,15 @@ function getLastWeekDate() {
   const formattedDate = year + "-" + month + "-" + day;
   return formattedDate;
 }
-
 export const previousWeek = getLastWeekDate();
 
+// checks server response
 export const checkServerResponse = (res) => {
   if (!res.ok) return Promise.reject(`An error with the Status Code ${res.status} has occurred`);
   return res.json();
 };
 
+// formats date recieved from api call to US format
 export const formatSearchResDate = (date) => {
   return new Date(date).toLocaleString("default", { year: "numeric", month: "long", day: "numeric" });
 };
