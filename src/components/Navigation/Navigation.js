19febe44ca10456ea179b1cf791f21be0054ca0d
mnunezsa <mnunezsa95@gmail.com>
2023-10-26 18:06:27 -0400

    update

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,26 +4,36 @@ import "./Navigation.css";
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
 function Navigation({ isLoggedIn, onLoginModal, onLogout }) {
-  const location = useLocation().pathname;
+  const reactLocation = useLocation();
+  const currentLocation = reactLocation.pathname;
+  const showActive = window.location.pathname;
 
   return (
     <div className="nav">
       <div className="nav__left-container">
-        <h1 className={`nav__logo ${location === "/saved-news" && "nav__logo-saved-news"}`}>NewsExplorer</h1>
+        <h1 className={`nav__logo ${currentLocation === "/saved-news" && "nav__logo-saved-news"}`}>NewsExplorer</h1>
       </div>
       <div className="nav__right-container">
         <div className="nav__container-links">
           <Link to="/">
-            <button className={`nav__button ${location === "/saved-news" && "nav__button-saved-news"}`}>Home</button>
+            <button className={`nav__button ${currentLocation === "/saved-news" && "nav__button-saved-news"} ${showActive === "/" && "nav__button-active"}`}>
+              Home
+            </button>
           </Link>
           {isLoggedIn && (
             <Link to="/saved-news">
-              <button className={`nav__button ${location === "/saved-news" && "nav__button-saved-news"}`}>Saved Articles</button>
+              <button
+                className={`nav__button  ${currentLocation === "/saved-news" && "nav__button-saved-news"} ${
+                  showActive === "/saved-news" && "nav__button-active-saved-news"
+                }`}
+              >
+                Saved Articles
+              </button>
             </Link>
           )}
           {isLoggedIn ? (
             <button
-              className={`nav__button nav__button-logout ${location === "/saved-news" && "nav__button-saved-news nav__button-logout-saved-news"}`}
+              className={`nav__button nav__button-logout ${currentLocation === "/saved-news" && "nav__button-saved-news nav__button-logout-saved-news"}`}
               onClick={onLogout}
             >
               Marlon
