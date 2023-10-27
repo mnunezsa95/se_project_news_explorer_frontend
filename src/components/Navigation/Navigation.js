1d3841bc5c688d876822028aebe86dfda0db0434
mnunezsa <mnunezsa95@gmail.com>
2023-10-27 08:59:07 -0400

    add active underline on nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,40 +6,30 @@ import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 function Navigation({ isLoggedIn, onLoginModal, onLogout }) {
   const reactLocation = useLocation();
   const currentLocation = reactLocation.pathname;
-  const showActive = window.location.pathname;
 
   return (
     <div className="nav">
       <div className="nav__left-container">
-        <h1 className={`nav__logo ${currentLocation === "/saved-news" && "nav__logo-saved-news"}`}>NewsExplorer</h1>
+        <h1 className={currentLocation === "/" ? "nav__logo" : "nav__logo-saved-news"}>NewsExplorer</h1>
       </div>
       <div className="nav__right-container">
         <div className="nav__container-links">
           <Link to="/">
-            <button className={`nav__button ${currentLocation === "/saved-news" && "nav__button-saved-news"} ${showActive === "/" && "nav__button-active"}`}>
-              Home
-            </button>
+            <button className={currentLocation === "/" ? "nav__button nav__button-active" : "nav__button-saved-news"}>Home</button>
           </Link>
           {isLoggedIn && (
             <Link to="/saved-news">
-              <button
-                className={`nav__button  ${currentLocation === "/saved-news" && "nav__button-saved-news"} ${
-                  showActive === "/saved-news" && "nav__button-active-saved-news"
-                }`}
-              >
+              <button className={currentLocation === "/saved-news" ? "nav__button-saved-news nav__button-active-saved-news" : "nav__button"}>
                 Saved Articles
               </button>
             </Link>
           )}
           {isLoggedIn ? (
-            <button
-              className={`nav__button nav__button-logout ${currentLocation === "/saved-news" && "nav__button-saved-news nav__button-logout-saved-news"}`}
-              onClick={onLogout}
-            >
+            <button className={currentLocation === "/" ? "nav__button-logout" : "nav__button-logout-saved-news"} onClick={onLogout}>
               Marlon
             </button>
           ) : (
-            <button className="nav__button nav__button-signin" onClick={onLoginModal}>
+            <button className="nav__button-signin" onClick={onLoginModal}>
               Sign in
             </button>
           )}
