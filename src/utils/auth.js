978794234f6650199df5a27dcdd55952c151160e
mnunezsa <mnunezsa95@gmail.com>
2023-11-09 11:18:27 -0500

    add signin and signup auth functions

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,25 @@
-const signIn = () => {};
+import { BASE_URL, checkServerResponse } from "./constants.js";
 
-const signUp = () => {};
+const signUp = ({ email, password, name }) => {
+  return fetch(`${BASE_URL}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, name }),
+  }).then(checkServerResponse);
+};
 
-export { signIn, signUp };
+const signIn = ({ email, password }) => {
+  return fetch(`${BASE_URL}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then(checkServerResponse);
+};
+
+const authorizeToken = (token) => {};
+
+export { signIn, signUp, authorizeToken };
