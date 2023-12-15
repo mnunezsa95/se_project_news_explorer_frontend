d9157fe0d8ba73bbc0b7e21e78e3ee6febe44f71
mnunezsa <mnunezsa95@gmail.com>
2023-12-15 12:53:13 -0500

    update profile pic

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,12 @@
 import { React } from "react";
-import placeholder_image from "../../images/placeholder_image.svg";
+import authorImage from "../../images/authorImage.png";
 import "./About.css";
 
 function About() {
   return (
     <section className="about">
       <div className="about__image-container">
-        <img className="about__image-author" src={placeholder_image} alt="author avatar"></img>
+        <img className="about__image-author" src={authorImage} alt="author avatar"></img>
       </div>
       <div className="about__info-container">
         <h2 className="about__title">About the author</h2>
@@ -15,10 +15,10 @@ function About() {
           engineer based in Boston, MA.
         </p>
         <p className="about__text">
-          Over the last eight months, I have been working towards my goal of breaking into the tech industry as an engineer. During my time at triple ten, I had
-          the opportunity of working with many different languages, frameworks, and tools such as, but not limited to: HTML, CSS, JavaScript, React.js, Node.js,
-          Express.js, MongoDB, Google Cloud, Babel, NPM, and Webpack. I also have some knowledge of other tools and languages such as SQL, Python, Parcel and
-          more.
+          In the past nine months, I've actively pursued my aspiration of entering the tech industry as an engineer. My experience at TripleTen has been
+          instrumental in honing my skills across a diverse array of languages, frameworks, and tools. Notably, I've adeptly worked with HTML, CSS, JavaScript,
+          React.js, Node.js, Express.js, MongoDB, Google Cloud, Babel, NPM, and Webpack. Additionally, I possess proficiency in other tools and languages,
+          including SQL, Python, Parcel, and more.
         </p>
       </div>
     </section>
